1432cb0c08a52bc444659edb9162cf500d4e4dfe
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-21 15:14:19 +0200

    add files
    add first upd

---
+++ src/index.js
@@ -0,0 +1,23 @@
+"use strict"
+const modal = document.querySelector('.modal');
+const visibleClassName = 'modal--visible';
+function openModal() {
+    modal.classList.add(visibleClassName);
+}
+modal.addEventListener('click', ({ target }) => {
+    if (target.dataset.action === 'close') {
+        modal.classList.remove(visibleClassName);
+    }
+});
+addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+        modal.classList.remove(visibleClassName);
+    }
+});
+// const outer = document.querySelector('.outer');
+// const inner = document.querySelector('.inner');
+// outer.addEventListener('mousemove', (event) => {
+//     const { x, y } = event;
+//     inner.style.translate = `${x - 10}px ${y - 10}px`;
+// });
+openModal();
